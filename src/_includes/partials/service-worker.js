a5308bc8ac033fe1f28e46bffbeb98c255993af4
marcentusch <marcentusch@hotmail.com>
2020-08-13 07:23:24 +0200

    removed test post

--- src/_includes/partials/service-worker.js
+++ src/_includes/partials/service-worker.js
@@ -10,7 +10,7 @@ const EXCLUDED_URLS = ["admin", ".netlify", "/contact"];
 const PRE_CACHE_URLS = ["/", "/blog"];
 
 // You might want to bypass a certain host
-const IGNORED_HOSTS = ["localhost", "unpkg.com"];
+const IGNORED_HOSTS = ["localhost"];
 
 /**
  * Takes an array of strings and puts them in a named cache store
--- src/posts/test-1.md
+++
@@ -1,7 +0,0 @@
----
-title: "How to add caching with service workers"
-date: "2020-10-10"
-tags: ["PWA", "Tips And Tricks", "Perfomance"]
----
-
-Working progress...
